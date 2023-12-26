666331987a9ff91fc479c5cf0763fbbccc36f1c1
mxcheung <mxcheung@users.noreply.github.com>
2023-12-27 10:03:06 +1100

    Update test_currency.py

--- pytest_boto3/test_currency.py
+++ pytest_boto3/test_currency.py
@@ -31,3 +31,32 @@ if usd_exists:
     print("The list contains a currency with code USD.")
 else:
     print("The list does not contain a currency with code USD.")
+
+
+class Currency:
+    def __init__(self, currency_code, account):
+        self.currency_code = currency_code
+        self.account = account
+
+# Sample list of currency objects
+currency_list = [
+    Currency("USD", "Account1"),
+    Currency("EUR", "Account2"),
+    Currency("GBP", "Account3"),
+    # ... add more currencies as needed
+]
+
+# Find the currency object with code "USD"
+usd_currency = None
+for currency in currency_list:
+    if currency.currency_code == "USD":
+        usd_currency = currency
+        break
+
+if usd_currency is not None:
+    print("Found the currency object with code USD:")
+    print(f"Currency Code: {usd_currency.currency_code}")
+    print(f"Account: {usd_currency.account}")
+else:
+    print("No currency object found with code USD.")
+
